41ac7b5309f4ddc73b097a396a7dd20320ccc533
lmvengeance <ranggapangestu0079@gmail.com>
2024-05-14 19:19:32 +0700

    first commit

---
+++ ingodwetrust.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Mohammad Rangga Pangestu";
+$nim = "21.11.4052";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>In God We Trust</title>
+</head>
+<body>
+    <h1>UTS Digital Bisnis</h1>
+    <p><?php echo $name ?></p>
+    <p><?php echo $nim ?></p>
+</body>
+</html>
